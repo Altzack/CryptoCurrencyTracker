2a0ae22a9bf4de17df8e099d1b21c6f55efdb9bd
zack altschuler <altschulerzack@yahoo.com>
2023-03-14 12:41:09 -0700

    fix

--- src/config.js
+++ src/config.js
@@ -1,16 +1,13 @@
 export default {
   API_KEY: '789af179-6a9f-48db-85e5-c243353fa492',
   ALPHA_KEY: '1I7VDVD0HOFGSF6L',
-  API_ENDPOINT:
-    'https://boiling-bastion-93969.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/',
+  API_ENDPOINT: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/',
   GRAPH_ENDPOINT:
     'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&interval=60min&',
   GRAPH_KEY: '1I7VDVD0HOFGSF6L',
-  META_ENDPOINT:
-    'https://boiling-bastion-93969.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?',
+  META_ENDPOINT: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?',
   GLOBAL_METRIC:
-    'https://boiling-bastion-93969.herokuapp.com/https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest?',
-  NEWS_ENDPOINT:
-    'https://boiling-bastion-93969.herokuapp.com/https://gnews.io/api/v4/search?',
+    'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest?',
+  NEWS_ENDPOINT: 'https://gnews.io/api/v4/search?',
   NEWS_TOKEN: '7fb6ae2c2ee561cec749142b4540ed45',
 };
